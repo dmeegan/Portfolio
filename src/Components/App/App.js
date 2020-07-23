55e456c6577e35778033ee40ed1ea6b8ddfb8247
dmeegan <dn_meegan@yahoo.com>
2020-07-23 15:07:44 -0400

    Fixed responsive topnavy and burger to not take up excess space and not transition out on screen size change.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Burger from '../Nav/Burger.js'
 import NavLinks from '../Nav/NavLinks.js'
@@ -8,8 +8,12 @@ function App() {
 
   const [navIsActive, setIsActive] = useState(false);
 
-  function toggleNavActive () {
+  function toggleNavActive() {
     setIsActive(navIsActive => !navIsActive)
+  }
+
+  function disableNav() {
+
   }
 
   return (
@@ -17,10 +21,16 @@ function App() {
       <div className="nav-container">
         <nav className="nav">
           <div className="logo">Logo Here</div>
-          <NavLinks navIsActive={navIsActive}/>
+          <NavLinks navIsActive={navIsActive} />
           <Burger navIsActive={navIsActive} toggleNavActive={toggleNavActive} />
         </nav>
       </div>
+        <section className="about-section">
+          <img className="headshot" src='headshot.jpg' title="Dan Meegan" alt="dan meegan headshot photo"/>
+          <h1>About Me</h1>
+          <p>I am a self-taught web developer currently based in Boston. I am focused on creating clean modern websites and web applications.</p>
+          <p>Please feel free to contact me with any opportunities.</p>
+        </section>
     </div>
   );
 }
