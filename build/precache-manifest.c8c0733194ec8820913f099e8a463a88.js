5cfd12524ec924a094733a9ad561993e7717edc0
dmeegan <dn_meegan@yahoo.com>
2020-09-18 17:59:36 -0400

    add build

---
+++ build/precache-manifest.c8c0733194ec8820913f099e8a463a88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68ffef2def5b010ee985a0cfa2eca14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e564539facab145a83",
+    "url": "/static/css/main.48ef77a4.chunk.css"
+  },
+  {
+    "revision": "e0fd9c8b9b69978ea88e",
+    "url": "/static/js/2.0b15655c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b15655c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e564539facab145a83",
+    "url": "/static/js/main.5217211e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "15c168539fb7109ce300574e7b4b0732",
+    "url": "/static/media/blackgrit.15c16853.png"
+  },
+  {
+    "revision": "9116408ec30c8e6378d437c07fd95987",
+    "url": "/static/media/hero-boston.9116408e.JPG"
+  },
+  {
+    "revision": "3e67bed838cd933911fa64a76a43624f",
+    "url": "/static/media/whitegrit.3e67bed8.png"
+  }
+]);
