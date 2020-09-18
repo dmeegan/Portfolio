98798edcb6bef0e4c001efc020ea0a68bb6c42ca
dmeegan <dn_meegan@yahoo.com>
2020-09-18 13:51:05 -0400

    1. Connect navLinks to sections for auto scrolling
    2. Address Accessability concerns of contrasting colors.
    
    3. Add more in the about me section
    
    4. Add distressed background svg.
    
    5. Add further atyling.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,21 +2,24 @@ import React from 'react';
 
 function Contact() {
     return (
-        <form className="contactForm" name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact"/>
-            <div>
-                <label for="contactName">Your Name </label> <input type="text" name="name" id="contactName" required placeholder="Enter your name"/><br></br>
-            </div>
-            <div>
-                <label for="contactEmail">Your Email </label> <input type="email" name="email" id="contactEmail" required placeholder="Enter your email"/><br></br>
-            </div>
-            <div>
-                <label for="contactMessage"> Message </label><textarea name="message" rows="10" id="contactMessage" required placeholder="Message"></textarea>
-            </div>
-            <div>
-                <button type="submit">Send</button>
-            </div>
-        </form>
+        <section id="contact-section">
+            <h2>Contact Me</h2>
+            <form className="contactForm" name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                <div>
+                    <label for="contactName">Your Name </label> <input type="text" name="name" id="contactName" required placeholder="Enter your name" /><br></br>
+                </div>
+                <div>
+                    <label for="contactEmail">Your Email </label> <input type="email" name="email" id="contactEmail" required placeholder="Enter your email" /><br></br>
+                </div>
+                <div>
+                    <label for="contactMessage"> Message </label><textarea name="message" rows="10" id="contactMessage" required placeholder="Message"></textarea>
+                </div>
+                <div>
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </section>
     )
 }
 
--- src/Components/Nav/NavLinks.js
+++ src/Components/Nav/NavLinks.js
@@ -4,9 +4,9 @@ function NavLinks(props) {
 
     return (
         <ul className={props.navIsActive ? 'navLinks navLinks-active' : 'navLinks'}>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Work</a></li>
-            <li><a href='#'>Contact</a></li>
+            <li><a href='#about-section'>About</a></li>
+            <li><a href='#portfolio-section'>Work</a></li>
+            <li><a href='#contact-section'>Contact</a></li>
         </ul>
     );
 }
