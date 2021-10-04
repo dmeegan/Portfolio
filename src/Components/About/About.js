19d9d6a791345aa9cc54935e5ba1a6ee3d9f6ee5
Daniel Meegan <dn_meegan@yahoo.com>
2021-10-03 20:46:34 -0400

    Update About.js
    Fix grammar mistake

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,7 @@ function About() {
                 <img className="headshot" src='./Images/headshot.jpg' title="Dan Meegan" alt="dan meegan headshot" />
                 </div>
                 <div className="am-text">
-                <p>I am a self-taught web developer currently based in Boston. With a Bachelors Degree in Engineering, I continue to use my STEM background and analytical skills in my web development work. My focused is on creating clean modern websites and web applications that solve real-world problems.</p>
+                <p>I am a self-taught web developer currently based in Boston. With a Bachelors Degree in Engineering, I continue to use my STEM background and analytical skills in my web development work. My focuse is on creating clean modern websites and web applications that solve real-world problems.</p>
                 <p>Working in the field of Engineering has taught me how to provide the best possible product, that meets a clients needs, before the scheduled deadline.</p>
                 <p>I have experience with HTML, CSS, JavaScript, React, and I am constantly learning new tools to add to my abilities.</p>
                 <p>Please feel free to {<a href='#contact-section'>contact</a>} me with any opportunities.</p>
